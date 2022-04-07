383b545441e40674adecc4d3e6b3b21c7cb30b9e
Pranay Bhaturkar <pbhaturkar5@gmail.com>
2022-04-07 10:00:21 +0530

    initial commit

---
+++ vmmart/android/app/src/main/kotlin/com/mart/vmmart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mart.vmmart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ vmmart/lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'VM-MART',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  bool isLoading = true;
+  String url = "https://vmmartpgroup.com/";
+  final _key = UniqueKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        WebView(
+          key: _key,
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onPageFinished: (String url) {
+            log('Page finished loading: $url');
+            setState(() {
+              isLoading = false;
+            });
+          },
+          javascriptChannels: <JavascriptChannel>{
+            _toasterJavascriptChannel(context),
+          },
+        ),
+        isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : const SizedBox()
+      ],
+    ));
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+}
